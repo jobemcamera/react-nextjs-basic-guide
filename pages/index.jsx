90e052f94b7ce3b6fee887db5bbf11179db639c3
jobe-lumni <jobecamera@gmail.com>
2023-06-20 19:25:07 -0300

    encapsulando Layout em _app para todas as paginas terem acesso. Removao de Layout da pagina Home

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,30 +1,27 @@
 import React from "react";
 import MeetupList from "../components/meetups/MeetupList";
-import Layout from "../components/layout/Layout";
 
 const DUMMY_MEETUPS = [
   {
-    id: 'm1',
-    title: 'Malta',
-    image: 'https://www.eurodicas.com.br/wp-content/uploads/2018/08/tudo-sobre-malta-1.jpg',
-    address: 'Valeta',
-    description: 'This is the first meetup'
+    id: "m1",
+    title: "Malta",
+    image:
+      "https://www.eurodicas.com.br/wp-content/uploads/2018/08/tudo-sobre-malta-1.jpg",
+    address: "Valeta",
+    description: "This is the first meetup",
   },
   {
-    id: 'm2',
-    title: 'Albania',
-    image: 'https://travellingbalkans.com/wp-content/uploads/2019/09/DJI_0853-HDR-1150x862.jpg',
-    address: 'Tirana',
-    description: 'This is the second meetup'
-  }
-]
+    id: "m2",
+    title: "Albania",
+    image:
+      "https://travellingbalkans.com/wp-content/uploads/2019/09/DJI_0853-HDR-1150x862.jpg",
+    address: "Tirana",
+    description: "This is the second meetup",
+  },
+];
 
 const HomePage = () => {
-  return (
-      <Layout>
-        <MeetupList meetups={DUMMY_MEETUPS}/>
-      </Layout>
-    );
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 };
 
 export default HomePage;
