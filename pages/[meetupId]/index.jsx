5ce742dd520fd7c97e015e0f7b5fc4553236ed55
jobe-lumni <jobecamera@gmail.com>
2023-06-20 19:43:45 -0300

    na pagina dinamica, chamando o componente novo MeetupDetail

---
+++ pages/[meetupId]/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://www.eurodicas.com.br/wp-content/uploads/2018/08/tudo-sobre-malta-1.jpg"
+      title="Malta"
+      address="Valeta"
+      description="This is a first meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
