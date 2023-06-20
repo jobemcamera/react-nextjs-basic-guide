35babe24c3a0efe1d7753c787c0f5ea7e5fa0c7b
jobe-lumni <jobecamera@gmail.com>
2023-06-20 17:17:10 -0300

    usando Link do Next para navegar entre paginas

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 // our-domain.com/
 
 function HomePage() {
-	return <h1>The Home Page</h1>
+  return (
+    <Fragment>
+      <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            Next.js Is A Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default HomePage;
