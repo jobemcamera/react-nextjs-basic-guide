3110ce7e1f4168f149373e8545b383836162883f
jobe-lumni <jobecamera@gmail.com>
2023-06-20 19:18:48 -0300

    trabalhando com os componentes Layout e MeetupList na pagina Home. Correcao da palavra 'address'

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import MeetupList from "../components/meetups/MeetupList";
+import Layout from "../components/layout/Layout";
 
 const DUMMY_MEETUPS = [
   {
     id: 'm1',
     title: 'Malta',
     image: 'https://www.eurodicas.com.br/wp-content/uploads/2018/08/tudo-sobre-malta-1.jpg',
-    adrres: 'Valeta',
+    address: 'Valeta',
     description: 'This is the first meetup'
   },
   {
     id: 'm2',
     title: 'Albania',
     image: 'https://travellingbalkans.com/wp-content/uploads/2019/09/DJI_0853-HDR-1150x862.jpg',
-    adrres: 'Tirana',
+    address: 'Tirana',
     description: 'This is the second meetup'
   }
 ]
 
 const HomePage = () => {
-  return <MeetupList meetups={DUMMY_MEETUPS}/>;
+  return (
+      <Layout>
+        <MeetupList meetups={DUMMY_MEETUPS}/>
+      </Layout>
+    );
 };
 
 export default HomePage;
