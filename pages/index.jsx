b53d41757d9cb3971a9c52382e413546e8d9ecf8
jobe-lumni <jobecamera@gmail.com>
2023-06-21 12:08:56 -0300

    alternativa para getStaticProps seria getServerSideProps para muitas requisicoes por segundo

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,6 +23,7 @@ const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
 
+
 // to load data before rendering the page
 export async function getStaticProps() {
   // fetch data from an API
@@ -33,5 +34,18 @@ export async function getStaticProps() {
     revalidate: 10 // X seconds to revalidate outdated data
   };
 }
+
+// an alternative to getStaticProps
+// it's running for every incoming requests (to many requests per second)
+// export async function getServerSideProps(context) {
+//   const req = context.req; // for authentication
+//   const res = context.res; // for authentication
+//   // fetch data from an API
+//   return { 
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }
 
 export default HomePage;
