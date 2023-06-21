a3cf9ce16429b87807ea6fc8e90b5be8b290e3a7
jobe-lumni <jobecamera@gmail.com>
2023-06-21 12:25:03 -0300

    fetch dados pela getStaticProps antes da pagina ser renderizada e indicando os paths possiveis da pagina com getStaticPaths

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -1,15 +1,52 @@
 import React from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   return (
     <MeetupDetail
-      image="https://www.eurodicas.com.br/wp-content/uploads/2018/08/tudo-sobre-malta-1.jpg"
-      title="Malta"
-      address="Valeta"
-      description="This is a first meetup"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 };
+
+// add all of id's in data
+export async function getStaticPaths() {
+  return {
+    fallback: false, // to indicate that I defined all supported paths here
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+  const meetupId = context.params.meetupId; // [meetupId]
+
+  return {
+    props: {
+      meetupData: {
+        id: "m1",
+        image:
+          "https://www.eurodicas.com.br/wp-content/uploads/2018/08/tudo-sobre-malta-1.jpg",
+        title: "Malta",
+        address: "Valeta",
+        description: "This is a first meetup",
+      },
+    },
+  };
+}
 
 export default MeetupDetails;
