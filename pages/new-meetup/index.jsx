a1329c33dfee740715b69ad52373235ef511a768
jobe-lumni <jobecamera@gmail.com>
2023-06-21 14:24:56 -0300

    enviando dados do form para o banco

--- pages/new-meetup/index.jsx
+++ pages/new-meetup/index.jsx
@@ -1,10 +1,25 @@
 // our-domain.com/new-meetup
 import React from 'react'
 import NewMeetupForm from '../../components/meetups/NewMeetupForm'
+import { useRouter } from 'next/router';
 
 const NewMeetupPage = () => {
-  const addMeetUpHandler = (enteredMeetupData) => {
-    console.log(enteredMeetupData)
+  const router = useRouter();
+
+  async function addMeetUpHandler(enteredMeetupData) {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push('/'); // return to the Home page
   }
 
   return (
